74f41a50c0c3174ff05ee40de78443ccf16d2bde
Henri Kontio <Henri.Kontio@solita.fi>
2017-05-17 17:28:53 +0300

    Add implementation for max-element and seq-max

--- src/recursion.clj
+++ src/recursion.clj
@@ -17,10 +17,14 @@
     (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (apply max a-seq)))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
   [:-])
